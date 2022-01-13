4a5642fb3f91175565d6ef8002c0de7c391d6a81
Harsha Kumari <kumariharsha33@gmail.com>
2022-01-13 15:25:06 +0530

    TextHelperTest | Comment out disable tag

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -4,20 +4,23 @@ package com.b.simple.design.business.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		char lastChar = str.charAt(-1);
-		char secondLastChar = str.charAt(-1);
-		String startingString = str.substring(0,str.length()-2);
+		int strLength = str.length();
+		if(strLength <2) return str;
+		char lastChar = str.charAt(strLength-1);
+		char secondLastChar = str.charAt(strLength-2);
+		String startingString = str.substring(0, strLength -2);
 		String finalstring = startingString + lastChar + secondLastChar;
 		return finalstring;
 	}
 
-	private String getReverseStr(String str, int strLen) {
-		String reverseStr;
-		reverseStr = (str.substring(strLen -1) + str.substring(strLen - 2, strLen -1));
-		return reverseStr;
-	}
 
 	public String truncateAInFirst2Positions(String str) {
-		return null;
+		int strLen = str.length();
+		if(strLen <= 2) {
+			return str.replaceAll("A","");
+		}
+		String first2characters = str.substring(0,2);
+		String remainingStr = str.substring(2);
+		return first2characters.replaceAll("A","") + remainingStr;
 	}
 }
